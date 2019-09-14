d76f9ac7c7ba01a53c7bcdfa474b41976d63aea0
TobiasLab <tobias.labidi@gmail.com>
2019-09-15 00:25:32 +0200

    Created first Test for the roman number "I", this only returns null.

--- src/test/java/no/kristiania/roman/RomanConverterTest.java
+++ src/test/java/no/kristiania/roman/RomanConverterTest.java
@@ -5,5 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanConverterTest {
+    @Test
+    void shouldConvertIto1() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int number) {
+        return null;
+    }
+}
 
 
