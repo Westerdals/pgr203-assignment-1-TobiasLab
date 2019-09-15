f5f3117c4c89146fdf4f4948ca27b5a49c33b132
TobiasLab <tobias.labidi@gmail.com>
2019-09-15 02:03:40 +0200

    All tests implemented.

--- src/test/java/no/kristiania/roman/RomanConverterTest.java
+++ src/test/java/no/kristiania/roman/RomanConverterTest.java
@@ -6,35 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanConverterTest {
     @Test
-    void shouldConvertIto1() {
-        assertEquals("I", RomanConverter.toRoman(1));
-    }
-
-    @Test
-    void shouldConvertIIto2() {
-        assertEquals("II", RomanConverter.toRoman(2));
-    }
-
-    @Test
-    void shouldConvertIIIto3() {
-        assertEquals("III", RomanConverter.toRoman(3));
-    }
-
-    @Test
     void shouldConvertIVto4() {
         assertEquals("IV", RomanConverter.toRoman(4));
-    }
-
-    @Test
-    void shouldConvertVto5() {
-        assertEquals("V", RomanConverter.toRoman(5));
-    }
-
-    @Test
-    void shouldCalculate6Through8() {
-        assertEquals("VI", RomanConverter.toRoman(6));
-        assertEquals("VII", RomanConverter.toRoman(7));
-        assertEquals("VIII", RomanConverter.toRoman(8));
     }
 
     @Test
@@ -43,14 +16,18 @@ class RomanConverterTest {
     }
 
     @Test
-    void shouldReturnXfor10() {
-        assertEquals("X", RomanConverter.toRoman(10));
-        assertEquals("XVIII", RomanConverter.toRoman(18));
+    void shouldCalculateSimpleDigits() {
+        assertEquals("MMMDCCCLXXXVIII", RomanConverter.toRoman(3888));
     }
 
     @Test
-    void shouldCalculateMultiplesOfTen() {
-        assertEquals("XXXVII", RomanConverter.toRoman(37));
+    void shouldCalculateFourDigits() {
+        assertEquals("CDXLIV", RomanConverter.toRoman(444));
+    }
+
+    @Test
+    void shouldCalculateNineLikeDigits() {
+        assertEquals("CMXCIX", RomanConverter.toRoman(999));
     }
 
 }
